d4f52f192f54a35f2b3b2e9418f9322d2ec8b00d
Hans Ulrich Niedermann <hun@n-dimensional.de>
2022-01-10 10:01:07 +0100

    Remove excessive code from foo/foo-linux.c
    We do not actually need this code for the purposes of
    looking for OP's linker error.

--- src/foo/foo-linux.c
+++ src/foo/foo-linux.c
@@ -18,15 +18,4 @@ void foo_host_func(void)
 	 ,LINUX_VERSION_MAJOR, LINUX_VERSION_PATCHLEVEL, LINUX_VERSION_SUBLEVEL
 #endif
   );
-#if defined(LINUX_VERSION_MAJOR) && defined(LINUX_VERSION_PATCHLEVEL) && defined(LINUX_VERSION_SUBLEVEL)
-  if (1) {
-    const unsigned int linux_version_code = LINUX_VERSION_CODE;
-    const unsigned int kernel_version =
-      KERNEL_VERSION(LINUX_VERSION_MAJOR, LINUX_VERSION_PATCHLEVEL, LINUX_VERSION_SUBLEVEL);
-    printf("LINUX_VERSION_CODE == %d == %#x %s KERNEL_VERSION == %d == %#x\n",
-           linux_version_code, linux_version_code,
-           (linux_version_code == kernel_version)?"==":"!=",
-           kernel_version, kernel_version);
-  }
-#endif
 }
