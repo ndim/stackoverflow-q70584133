e2f970773f27e30e7ae00e80ac63b72f290d320e
Hans Ulrich Niedermann <hun@n-dimensional.de>
2022-01-10 08:54:40 +0100

    main.c: Only call libbar's bar_func() if you uncomment a line
    Only call libbar's bar_func() from main.c if the user uncomments
    the src/main.c line
    
        // #define MAIN_DIRECTLY_USES_LIBBAR
    
    which will make `main.o` link to libbar's bar_func(), which in
    turn makes it reasonable to require uncommenting the
    
        # main_LDADD += libbar.la
    
    line in src/Makefile.am as well.

--- src/main.c
+++ src/main.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
+// #define MAIN_DIRECTLY_USES_LIBBAR
+
+#ifdef MAIN_DIRECTLY_USES_LIBBAR
 #include "bar/bar.h"
+#endif
+
 #include "foo/foo.h"
 
 int main()
 {
   printf("%s: %s\n", "main", HOST_TRIPLE);
+
+#ifdef MAIN_DIRECTLY_USES_LIBBAR
   bar_func();
+#endif
+
   foo_func();
   return 0;
 }
